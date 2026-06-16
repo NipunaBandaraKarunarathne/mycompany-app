bf134a2fd72b0cef5135ee38910c8c06da940286
Nipuna Bandara Karunarathne <66937343+NipunaBandaraKarunarathne@users.noreply.github.com>
2026-06-16 20:37:19 +0530

    sucess and error response

---
+++ order/src/main/java/com/example/order/common/ErrorOrderResponse.java
@@ -0,0 +1,15 @@
+package com.example.order.common;
+
+public class ErrorOrderResponse implements orderResponse {
+
+    private final String errorMessage;
+
+    public ErrorOrderResponse(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+}
--- order/src/main/java/com/example/order/common/SuccessOrderResponse.java
+++ order/src/main/java/com/example/order/common/SuccessOrderResponse.java
@@ -1,5 +1,18 @@
 package com.example.order.common;
 
+
+
+import com.example.order.dto.OrderDTO;
+
+import lombok.Getter;
+
+@Getter
 public class SuccessOrderResponse implements orderResponse{
+
+    private final OrderDTO order;
+
+    public SuccessOrderResponse(OrderDTO order) {
+        this.order = order;
+    }
 
 }
